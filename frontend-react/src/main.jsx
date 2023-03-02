0cf5a8eeeda55be64b203cbab85581a38b75462a
H-isaac23 <dev.isaac23@gmail.com>
2023-03-02 17:47:43 +0800

    refactor routes

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./routes/Login/Login";
+import Home from "./routes/Home/Home";
+import UserProfile from "./routes/UserProfile/UserProfile";
+import Settings from "./routes/Settings/Settings";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ContactUs from "./routes/ContactUs/Contact";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/userProfile",
+    element: <UserProfile />,
+  },
+  {
+    path: "/settings",
+    element: <Settings />,
+  },
+  {
+    path: "/contact",
+    element: <ContactUs />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- frontend-react/src/routes/Home/Home.jsx
+++ frontend-react/src/routes/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Leaderboards from "../components/Leaderboards/Leaderboards";
-import Profile from "../components/Profile/Profile";
-import BattleCharacter from "../components/BattleCharacter/BattleCharacter";
+import Leaderboards from "../../components/Leaderboards/Leaderboards";
+import Profile from "../../components/Profile/Profile";
+import BattleCharacter from "../../components/BattleCharacter/BattleCharacter";
 import "./Home.css";
-import bg from "../assets/img/4455.jpg";
+import bg from "../../assets/img/4455.jpg";
 
 export default function Home() {
   return (
